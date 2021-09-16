6a84eeaad321a755d1ae3868b565310e3dfea91e
SydMontague <sydmontague@phoenix-staffel.de>
2021-09-16 15:01:03 +0200

    Add project files.

---
+++ DSCSPluginTemplate/DSCSPluginTemplate.cpp
@@ -0,0 +1,31 @@
+﻿#include "DSCSPluginTemplate.h"
+#include <windows.h>
+#include <modloader/plugin.h>
+
+void TestFunction() {
+	MessageBoxA(NULL, "Custom Function Called", "Success!", MB_OK);
+}
+
+class TemplatePlugin : public BasePlugin {
+public:
+	using BasePlugin::BasePlugin;
+	void onEnable();
+	PluginInfo getPluginInfo();
+
+};
+
+void TemplatePlugin::onEnable() {
+	this->modLoader->addSquirrelFunction("DSCSModLoader", "TestFunction", SQUIRREL_AWAY(TestFunction));
+}
+
+PluginInfo TemplatePlugin::getPluginInfo() {
+	PluginInfo info;
+	info.apiVersion = { 0, 0, 0 };
+	info.version = { 1, 0, 0 };
+	info.name = "Template Plugin";
+	return info;
+}
+
+extern "C" __declspec(dllexport) TemplatePlugin * getPlugin(DSCSModLoader * modLoader) {
+	return new TemplatePlugin(modLoader);
+}
